50e47fbccf9d206677462e3d13a476a38f7bfabb
Tobimayer17 <tobimayer5@gmail.com>
2023-06-15 16:44:46 +0200

    registrieren.js aktualisiert aber noch nicht richtig

--- deinNachbar-war/WebContent/js/registrieren.js
+++ deinNachbar-war/WebContent/js/registrieren.js
@@ -1,10 +1,39 @@
 // Tobi
 
+/*
 document.addEventListener("DOMContentLoaded", init);
 
-//function init() {
- // document.getElementById("regbutton").addEventListener("click", checkPasswordMatch);
-//}
+function init() {
+  document.getElementById("registrierenButton").addEventListener("click", checkPasswordMatch);
+}
+
+function checkPasswordMatch(event) {
+  
+  event.preventDefault(); // Verhindert das Absenden des Formulars
+  
+  var password = document.getElementById("passwort").value;
+  var confirmPassword = document.getElementById("passwortwdh").value;
+  var registerButton = document.getElementById("registrierenButton");
+
+  if (password !== confirmPassword) {
+    registerButton.disabled = true;
+    
+    document.getElementById("meinTest").innerHTML = "Die Passwörter stimmen nicht überein!";
+  } else {
+    registerButton.disabled = false;
+    document.getElementById("meinTest").innerHTML = ""; // Entfernt die Fehlermeldung
+     // Überprüfe, ob das Formular korrekt ausgefüllt ist
+    event.target.form.submit(); // Formular wird gesendet
+    
+  }
+}
+*/
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  document.getElementById("registrierenButton").addEventListener("click", checkPasswordMatch);
+}
 
 function checkPasswordMatch(event) {
   event.preventDefault(); // Verhindert das Absenden des Formulars
@@ -12,13 +41,33 @@ function checkPasswordMatch(event) {
   var password = document.getElementById("passwort").value;
   var confirmPassword = document.getElementById("passwortwdh").value;
   var registerButton = document.getElementById("registrierenButton");
+  var errorMessage = document.getElementById("meinTest");
 
   if (password !== confirmPassword) {
     registerButton.disabled = true;
-    alert("Die Passwörter stimmen nicht überein!");
+    errorMessage.innerHTML = "Die Passwörter stimmen nicht überein!";
   } else {
     registerButton.disabled = false;
-    // Hier kannst du weitere Logik hinzufügen, um das Formular zu senden oder andere Aktionen auszuführen
-    document.getElementById("registrierung").submit(); // Beispiel: Formular wird automatisch gesendet
+    errorMessage.innerHTML = ""; // Entfernt die Fehlermeldung
+  }
+
+  if (isFormValid()) {
+    event.target.form.submit(); // Formular wird gesendet
   }
 }
+
+function isFormValid() {
+  var form = document.getElementById("registrierenForm");
+
+  // Führe hier die Validierung für andere Felder des Formulars durch
+  // Zum Beispiel: Überprüfung von Name, E-Mail usw.
+
+  // Return true, wenn das Formular gültig ist, andernfalls false
+  return form.checkValidity() && !document.getElementById("meinTest").innerHTML;
+}
+
+
+
+
+
+  
